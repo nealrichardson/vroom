dfa5f20798be8438635e5aea351fd4022bc88abb
Jim Hester <james.f.hester@gmail.com>
2020-03-26 08:56:39 -0400

    Use unicode for fopen

--- src/delimited_index_connection.cc
+++ src/delimited_index_connection.cc
@@ -13,6 +13,8 @@
 #include "spdlog/spdlog.h"
 #endif
 
+#include "unicode_fopen.h"
+
 using namespace vroom;
 
 delimited_index_connection::delimited_index_connection(
@@ -41,7 +43,7 @@ delimited_index_connection::delimited_index_connection(
   filename_ = Rcpp::as<std::string>(Rcpp::as<Rcpp::Function>(
       Rcpp::Environment::namespace_env("vroom")["vroom_tempfile"])());
 
-  std::FILE* out = std::fopen(filename_.c_str(), "wb");
+  std::FILE* out = unicode_fopen(filename_.c_str(), "wb");
 
   auto con = R_GetConnection(in);
 
---
+++ src/unicode_fopen.h
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+#ifdef _WIN32
+#include <Rinternals.h>
+#include <windows.h>
+#endif
+
+// This is needed to support wide character paths on windows
+inline FILE* unicode_fopen(const char* path, const char* mode) {
+  FILE* out;
+#ifdef _WIN32
+  // First conver the mode to the wide equivalent
+  // Only usage is 2 characters so max 8 bytes + 2 byte null.
+  wchar_t mode_w[10];
+  MultiByteToWideChar(CP_UTF8, 0, mode, -1, mode_w, 9);
+
+  // Then convert the path
+  wchar_t* buf;
+  size_t len = MultiByteToWideChar(CP_UTF8, 0, path, -1, NULL, 0);
+  if (len <= 0) {
+    error("Cannot convert file to Unicode: %s", path);
+  }
+  buf = (wchar_t*)R_alloc(len, sizeof(wchar_t));
+  if (buf == NULL) {
+    error("Could not allocate buffer of size: %ll", len);
+  }
+
+  MultiByteToWideChar(CP_UTF8, 0, path, -1, buf, len);
+  out = _wfopen(buf, mode_w);
+#else
+  out = fopen(path, mode);
+#endif
+
+  return out;
+}
