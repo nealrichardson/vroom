68a2f01f14bdec8c6962f88a0315732684e74162
Neal Richardson <neal.p.richardson@gmail.com>
2020-08-07 09:00:26 -0700

    Add arrow benchmark for taxi_multiple

---
+++ inst/bench/taxi_multiple/arrow-arrow.R
@@ -0,0 +1,8 @@
+({library(arrow); library(dplyr)})
+x <- open_dataset(dirname(file[1]))
+print(x)
+a <- head(x)
+b <- tail(x)
+c <- x[sample(NROW(x), 100), ]
+d <- filter(x, payment_type == "UNK") %>% collect(as_data_frame = FALSE)
+e <- group_by(x, payment_type) %>% select(tip_amount) %>% collect() %>% summarise(avg_tip = mean(tip_amount))
